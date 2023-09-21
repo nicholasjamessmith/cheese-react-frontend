3efaa67e59019f04016140967a09e4170f7e2b7a
nicholasjamessmith <njsmith6@gmail.com>
2023-09-20 22:24:28 -0400

    show page working

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,24 @@
+import { Link, useLoaderData, Form } from "react-router-dom";
+
+
 function Show(props) {
-  return <h1>Show</h1>
+  const post = useLoaderData();
+  
+  const div = {
+    textAlign: "center",
+    border: "3px solid green",
+    width: "80%",
+    margin: "30px auto",
+  };
+  
+  return (
+    <div style={div}>
+      <h1>{post.name}</h1>
+      <h2>{post.type}</h2>
+      <h2>{post.origin_country}</h2>
+      <Link to="/">Back to Index</Link>
+    </div>
+  )   
 }
 
 export default Show
